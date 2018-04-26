9ee8ed5f48e4a62828b81ae5608d3e5ab19e1705
ih_hira <imtiyaz.aust@gmail.com>
2018-04-26 19:30:29 +0600

    Initial Commit

---
+++ teletalk_number/split.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+string data;
+ifstream infile;
+
+void split(string filename)
+{
+    ofstream outfile;
+    outfile.open(filename.c_str());
+    long long c = 1000000;
+    while(c--)
+    {
+        infile>>data;
+        data="015"+data;
+        outfile<<data<<endl;
+    }
+    outfile.close();
+}
+int main ()
+{
+    infile.open("phone.txt");
+    char t[10];
+    for(int i=1;i<=100;i++)
+    {
+        sprintf(t,"%d",i);
+        string num;
+        ostringstream convert;
+        convert << i;
+        num = convert.str();
+        string filename = "tel_num"+num+".txt";
+        split(filename);
+    }
+}
+
+
